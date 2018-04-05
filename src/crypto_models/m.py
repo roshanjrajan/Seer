550b4e861fa9ebb59707970cb61db1bb1925f95c
alex <A.Bieniek789@gmail.com>
2018-04-04 22:58:56 -0500

    made working model as proof of concept, transform attributes for better model

--- src/crypto_models/m.py
+++ src/crypto_models/m.py
@@ -1,4 +1,4 @@
-# https://dashee87.github.io/deep%20learning/python/predicting-cryptocurrency-prices-with-deep-learning/
+#https://github.com/dashee87/blogScripts/blob/master/Jupyter/2017-11-20-predicting-cryptocurrency-prices-with-deep-learning.ipynb
 
 import pandas as pd
 import numpy as np
@@ -13,35 +13,62 @@ from keras.layers import Dropout
 
 ''' Putting data into a dataframe '''
 # slug,symbol,name,date,ranknow,open,high,low,close,volume,market,close_ratio,spread
-df = pd.read_csv('test_data/bitcoin.csv')
-# make a new column turning date strings into numerical times
-df['time'] = df.apply(lambda t: time.mktime(datetime.strptime(t['date'], '%Y-%m-%d').timetuple()), axis=1)
-df = df.sort_values(by=['name','time'])
-df = df[['open','high','low','close','volume','close_ratio','spread']] #,'time']] sorted by time, don't use in modeling?
+all_data = pd.read_csv('test_data/bitcoin.csv')
+# make a new column turning date strings into numerical times,
+# sort by name and time, then drop non-numerical attributes
+all_data['time'] = all_data.apply(lambda t: time.mktime(datetime.strptime(t['date'], '%Y-%m-%d').timetuple()), axis=1)
+all_data = all_data.sort_values(by=['name','time'])
+all_data = all_data.drop(['slug','symbol','name','date','ranknow'], 1)
 
-''' moving from dataframe to numpy stuff '''
-window_len = 10; windows = []
-for i in range(len(df)-window_len):
-	windows.append(df[i:(i+10)].copy())
+# make train test split: train on x% of data before a date, test on data after
+train_percentage = .8
+tpth_percentile_time = np.percentile(np.asarray(all_data['time']), train_percentage*100)
+train_data, test_data = all_data[all_data['time']<tpth_percentile_time], all_data[all_data['time']>=tpth_percentile_time]
+# drop time  column
+train_data = train_data.drop('time',1)
+test_data = test_data.drop('time',1)
+
+''' creating windows of timeseries data '''
+window_len = 10
+
+lstm_train_input = []
+for i in range(len(train_data)-window_len):
+	w = train_data[i:(i+window_len)].copy()
+	'''for col in ['close','volume']:
+		w.loc[:,col] = w[col]/w[col].iloc[0]-1'''
+	lstm_train_input.append(w)
+lstm_train_output = \
+  (train_data['open'][window_len:].values/train_data['open'][:-window_len].values)-1
+
+lstm_test_input = []
+for i in range(len(test_data)-window_len):
+	w = test_data[i:(i+window_len)].copy()
+	'''for col in ['close','volume']:
+		w.loc[:,col] = w[col]/w[col].iloc[0]-1'''
+	lstm_test_input.append(w)
+lstm_test_output = \
+  (test_data['open'][window_len:].values/test_data['open'][:-window_len].values)-1
+
+''' putting input windows into numpy arrays '''
+lstm_train_input = [np.array(window) for window in lstm_train_input]
+lstm_train_input = np.array(lstm_train_input)
+lstm_test_input = [np.array(window) for window in lstm_test_input]
+lstm_test_input = np.array(lstm_test_input)
 
 
-# expects inputs to be a 3D dataframe
-# first dimension - list of dataframes
-# second dimension - list of currency data entries
-# third dimension - list of values for an entry
 def build_model(inputs, output_size, neurons, activ_func = "linear",
                 dropout =0.25, loss="mae", optimizer="adam"):
     model = Sequential()
 
-    model.add(LSTM(neurons, input_shape=(10,7)))#inputs.shape[1], inputs.shape[2])))
+    model.add(LSTM(neurons, input_shape=(inputs.shape[1], inputs.shape[2])))
     model.add(Dropout(dropout))
-
     model.add(Dense(units=output_size))
     model.add(Activation(activ_func))
 
     model.compile(loss=loss, optimizer=optimizer)
     return model
 
-m = build_model(windows, output_size=1, neurons=20)
-out = (windows['open'][window_len:].values/windows['open'][:-window_len].values)-1
-hist = m.fit(templist, out, epochs=50, batch_size=1, verbose=2, shuffle=True)
+# build the model
+m = build_model(lstm_train_input, output_size=1, neurons=20)
+out = (train_data['open'][window_len:].values/train_data['open'][:-window_len].values)-1
+hist = m.fit(lstm_train_input, lstm_train_output, epochs=50, batch_size=1, verbose=2, shuffle=True)
