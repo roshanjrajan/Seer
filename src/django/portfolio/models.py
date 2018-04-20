27ede698591062b67a0e5c2d5b0e4a7429220bfb
alex <A.Bieniek789@gmail.com>
2018-04-20 17:12:07 -0500

    updated transaction table fields

--- src/django/portfolio/models.py
+++ src/django/portfolio/models.py
@@ -6,18 +6,11 @@ from django.db import models
 
 class Transaction(models.Model):
     # account = models.ForeignKey(User?????)
-    action = models.CharField(max_length=10)
-    date = models.DateField('transaction date')
-    security = models.CharField(max_length=10, blank=True)
-    shares = models.DecimalField(decimal_places=2, max_digits=10, null=True)
-    price = models.DecimalField(decimal_places=2, max_digits=10, null=True)
-    commission = models.DecimalField(decimal_places=2, max_digits=10,
-                                     null=True)
-    cash_amount = models.DecimalField(decimal_places=2, max_digits=10,
-                                      null=True)
-    sec_fee = models.DecimalField(decimal_places=2, max_digits=10, null=True)
-    split_ratio = models.DecimalField(decimal_places=2, max_digits=5,
-                                      null=True)
+    action = models.CharField(max_length=1, blank=False)
+    currency = models.CharField(max_length=20, blank=False)
+    amount = models.IntegerField(blank=False)
+    value = models.DecimalField(decimal_places=2, max_digits=10, null=False)
+    time = models.IntegerField(blank=False)
 
     def __unicode__(self):
 		return self.action + ' ' + str(self.shares) + ' ' + self.security
