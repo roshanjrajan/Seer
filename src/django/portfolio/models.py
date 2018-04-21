ef3eb279e35fcf04e8fe6882acd4e6daeb05565a
alex <A.Bieniek789@gmail.com>
2018-04-20 23:12:50 -0500

    removed date column in transactions, it's redundant

--- src/django/portfolio/models.py
+++ src/django/portfolio/models.py
@@ -8,7 +8,6 @@ from django.contrib.auth.models import User
 class Transaction(models.Model):
     account = models.ForeignKey(User, related_name='members', null=True)
     action = models.CharField(max_length=10)
-    date = models.DateField('Date')
     currency = models.CharField(max_length=20, null=True)
     number_of_stocks = models.DecimalField(decimal_places=2, max_digits=10, null=True)
     price = models.DecimalField(decimal_places=2, max_digits=10, null=True)
