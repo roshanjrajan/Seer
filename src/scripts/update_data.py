abac65823e787b864b047637aa4b339be83f956e
Debian <admin@ip-172-26-8-242.us-east-2.compute.internal>
2018-04-22 20:35:36 +0000

    small version update for update_data script

--- src/scripts/update_data.py
+++ src/scripts/update_data.py
@@ -1,9 +1,11 @@
 import urllib.request, json, psycopg2
-c_url="https://min-api.cryptocompare.com/data/histohour?fsym=BTC&tsym=USD&limit=1"
-values = {'fsym': "BTC", "tsym" : "USD", "limit" : 2000}
+c_url="https://min-api.cryptocompare.com/data/histohour?fsym=BTC&tsym=USD&limit=10"
+values = {'fsym': "BTC", "tsym" : "USD", "limit" : 10}
+currency = ['BTC', 'ETH', 'LTC']
 in_value = urllib.parse.urlencode(values).encode("ascii")
 conn = psycopg2.connect("host=localhost dbname=crypto user=postgres")
 cur = conn.cursor()
+
 
 def get_data(curr_url):
     print(curr_url)
@@ -12,26 +14,29 @@ def get_data(curr_url):
         data = json.loads(url.read().decode())
         return data
 
-def get_timestamp(data):
-    return data["Data"][0]["time"]
-
-def insert_db(data):
+def insert_db(data, currency):
     for item in data["Data"]:
         query =  "INSERT INTO bitcoin (CURRENCY, OPEN, CLOSE, HIGH, LOW, TIME, VOLUMETO, VOLUMEFROM) VALUES (%s, %s, %s, %s, %s, %s, %s, %s);"
-        values = ("BTC", item["open"], item["close"], item["high"], item["low"], item["time"], item["volumeto"], item["volumefrom"])
+        values = (currency, item["open"], item["close"], item["high"], item["low"], item["time"], item["volumeto"], item["volumefrom"])
         cur.execute(query, values)
     conn.commit()
 
-def check_for_zero(data):
-    for item in data["Data"]:
-        if(item["open"] == 0):
-            print (item["time"])
-            return True
+def remove_zeros_and_dupes():
+    query = "DELETE FROM bitcoin a USING ( SELECT MIN(ctid) as ctid, time, currency FROM bitcoin GROUP BY currency, time HAVING COUNT(*) > 1) b WHERE a.currency = b.currency AND a.time = b.time AND a.ctid <> b.ctid "
+    cur.execute(query)
+    
+    query = "DELETE FROM bitcoin where open = 0 or close = 0 or high = 0 or low = 0 or volumeto = 0 or volumefrom = 0"
+    cur.execute(query)
+    
+    conn.commit()
 
-
-data = get_data(c_url)
-start_timestamp = get_timestamp(data)
-
-insert_db(data)
-
+old = 'BTC'
+for curr in currency:
+    values['fsym'] = curr
+    c_url = c_url.replace(old, curr) 
+    old = curr
+    data = get_data(c_url)
+    insert_db(data, curr)
+    
+remove_zeros_and_dupes()
 
