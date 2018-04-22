2fd66583fb51fcd9e911adbb58826b3e7d84d191
Debian <admin@ip-172-26-8-242.us-east-2.compute.internal>
2018-04-22 18:17:19 +0000

    building models and predicting stuff now works with databases

--- src/crypto_models/build_lstm_model.py
+++ src/crypto_models/build_lstm_model.py
@@ -15,10 +15,20 @@ from keras.layers import Dropout
 from keras.models import load_model
 
 WINDOW_LEN = 10
+MAX_TRAIN_SIZE = 1000
+NEURON_COUNT = 20
+NUM_EPOCHS = 25
+TRAINING_ATTRIBUTES = ['open', 'high', 'low', 'close', 'volumefrom', 'volumeto']
+COLS_TO_NORMALIZE = ['open','high','low','close']
 
-def normalize_window(window, columnNames):
-	for col in columnNames:
-			w.loc[:,col] = pw[col]/(w[col].iloc[0]+1) - 1
+# normalize each value in the window to fit the value
+# of the first value in the window
+def normalize_window_df(window, columnNames):
+    for col in columnNames:
+    	window.loc[:,col] = window[col]/(window[col].iloc[0]+1) - 1
+
+def discover_features(frame):
+    frame['volatility'] = (frame['high']-frame['low'])/(frame['close']+1)
 
 def main():
 	# parsing inputs
@@ -32,57 +42,40 @@ def main():
 		print "usage: python ", sys.argv[0], "-c nameOfCurrency -o outputmodel.h5"
 		exit()
 
-	''' Stuff about our data for querying '''
-	trainingAttributes = ['open', 'high', 'low', 'close', 'volumefrom', 'volumeto']
-
 	''' SQL Query for Training Data '''
         conn = psycopg2.connect("host=localhost dbname=crypto user=postgres")
 	cursor = conn.cursor()
-	query = "SELECT " + ", ".join(trainingAttributes) + " FROM bitcoin"\
-	        + " WHERE Currency=\'"+currencyname+"\'"\
+	query = "SELECT " + ", ".join(TRAINING_ATTRIBUTES) + " FROM bitcoin"\
+	        + " WHERE UPPER(Currency)=UPPER(\'"+currencyname+"\')"\
 	        + " ORDER BY time ASC"
 	cursor.execute(query)
 	results = cursor.fetchall()
-	all_data = pd.DataFrame(results, columns=trainingAttributes)
+	all_data = pd.DataFrame(results, columns=TRAINING_ATTRIBUTES)
         conn.close()
 
 	# feature discovery
-	all_data['volatility'] = (all_data['high']-all_data['low'])/all_data['close']
-
+	discover_features(all_data)
 
 	# make train test split: train on x% of data before a date, test on data after
 	train_percentage = 1.0; train_num = int(len(all_data)*train_percentage)
-	train_data, test_data = all_data[0:train_num], all_data[train_num:]
+        train_data = all_data[-MAX_TRAIN_SIZE:]
+        del(all_data)
 
-	''' creating windows of timeseries data '''
-	# per-window attribute modification
-		# normalize the specified columns to the value of the first entry
-	#['high','low','close','open_high_diff','close_high_diff']
-	cols_to_normalize = ['open','high','low','close']
-
+	''' creating windows of timeseries data and normalising windows '''
 	lstm_train_input = []
 	for i in range(len(train_data)-WINDOW_LEN):
 		w = train_data[i:(i+WINDOW_LEN)].copy()
 		lstm_train_input.append(w)
-	for w in lstm_train_input: normalize_window(w, cols_to_normalize)
+	for i in range(len(lstm_train_input)):
+            print(type(lstm_train_input[i]))
+            normalize_window_df(lstm_train_input[i], COLS_TO_NORMALIZE)
 	lstm_train_output = \
 	 (train_data[WINDOW_LEN:].values/(train_data[:-WINDOW_LEN].values+1))-1 # normalized
-	# train_data['close'][WINDOW_LEN:].values unnormalized
-
-	lstm_test_input = []
-	for i in range(len(test_data)-WINDOW_LEN):
-		w = test_data[i:(i+WINDOW_LEN)].copy()
-		lstm_test_input.append(w)
-	for w in lstm_test_input: normalize_window(w, cols_to_normalize) 
-	lstm_test_output = \
-	(test_data[WINDOW_LEN:].values/(test_data[:-WINDOW_LEN].values+1))-1 # normalized
-	# test_data['close'][WINDOW_LEN:].values unnormalized
+        del(train_data)
 
 	''' putting input windows into numpy arrays '''
 	lstm_train_input = [np.array(window) for window in lstm_train_input]
 	lstm_train_input = np.array(lstm_train_input)
-	lstm_test_input = [np.array(window) for window in lstm_test_input]
-	lstm_test_input = np.array(lstm_test_input)
 
 	def build_model(inputs, output_size, neurons, activ_func = "linear",
 	                dropout =0.25, loss="mae", optimizer="adam"):
@@ -97,9 +90,8 @@ def main():
 	    return model
 
 	# build the model
-	m = build_model(lstm_train_input, output_size=np.shape(lstm_test_input)[2], neurons=20)
-	out = (train_data[WINDOW_LEN:].values/(train_data[:-WINDOW_LEN].values+1))-1
-	hist = m.fit(lstm_train_input, lstm_train_output, epochs=10, batch_size=1, verbose=2, shuffle=True)
+	m = build_model(lstm_train_input, output_size=np.shape(lstm_train_input)[2], neurons=NEURON_COUNT)
+        hist = m.fit(lstm_train_input, lstm_train_output, epochs=NUM_EPOCHS, batch_size=1, verbose=2, shuffle=True)
 
 	# save the model
 	m.save(outputfilename)
--- src/crypto_models/pred_with_lstm_model.py
+++ src/crypto_models/pred_with_lstm_model.py
@@ -4,6 +4,7 @@ import numpy as np
 import time
 from datetime import datetime
 import matplotlib.pyplot as plt
+import psycopg2
 
 from keras.models import Sequential
 from keras.layers import Activation, Dense
@@ -11,25 +12,43 @@ from keras.layers import LSTM
 from keras.layers import Dropout
 from keras.models import load_model
 
-import build_lstm_model
+from build_lstm_model import *
+
+def normalize_window_np(window, indices):
+    ret = window/(window[:][0]+1)-1
+    return ret
 
 # parsing inputs
 try:
 	mflagidx = sys.argv.index('-m')
 	inputmodelname = sys.argv[mflagidx+1]
-	iflagidx = sys.argv.index('-i')
-	inputsamplename = sys.argv[iflagidx+1]
+	cflagidx = sys.argv.index('-c')
+	currencyname = sys.argv[cflagidx+1]
 	dflagidx = sys.argv.index('-d')
 	daycount = int(sys.argv[dflagidx+1])
 
 except ValueError:
 	# malformed command line arguments, print usage and exit
-	print "usage: python ", sys.argv[0], "-m inputmodel.h5 -i inputsample.csv -d daycount"
+	print "usage: python ", sys.argv[0], "-m inputmodel.h5 -c currencyname -d daycount"
 	exit()
-
-# load sample
-sample = np.genfromtxt(inputsamplename,delimiter=',')
+# get most recent crypto data from database with SQL query
+conn = psycopg2.connect("host=localhost dbname=crypto user=postgres")
+cursor = conn.cursor()
+query = "SELECT " + ", ".join(TRAINING_ATTRIBUTES) + " FROM ("\
+        + "SELECT " + ", ".join(TRAINING_ATTRIBUTES)+", time FROM bitcoin "\
+        + " WHERE UPPER(Currency)=UPPER(\'"+currencyname+"\')"\
+        + " ORDER BY Time DESC LIMIT 10"\
+        + ") as sub "\
+        + " ORDER BY time ASC"
+cursor.execute(query)
+results = cursor.fetchall()
+results = [[float(t) for t in a] for a in results]
+df = pd.DataFrame(results, columns=TRAINING_ATTRIBUTES)
+discover_features(df)
+sample = df.as_matrix().copy()
 sample = np.expand_dims(sample, axis=0)
+print sample
+conn.close()
 
 # load model
 model = load_model(inputmodelname)
@@ -38,10 +57,10 @@ preds = []
 
 # predict the metrics for the given cryptocurrency for the subsequent d days
 for d in range(daycount):
-	# predict values, unnormalize the prediction
-	normalized_window = normalize_window(sample)
-	pred = model.predict(sample)
-	pred = (pred+1) * (np.average(sample,axis=1) - 1)
+	# normalize input, predict values, unnormalize the prediction
+        normalized_window = normalize_window_np(sample, [TRAINING_ATTRIBUTES.index(t) for t in COLS_TO_NORMALIZE])
+        pred = model.predict(normalized_window)
+        pred = (pred+1) * (np.average(sample, axis=1) - 1)
 	# use prediction to modify input window, and add the prediction to the list of predictions
 	preds.append(pred)
 
