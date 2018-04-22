2999c1f21b6ea55a6e6fe09076952fe3c4ae4a1c
alex <A.Bieniek789@gmail.com>
2018-04-22 15:26:28 -0500

    readded plot_prices.py

---
+++ src/crypto_models/plot_prices.py
@@ -0,0 +1,68 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from pred_with_lstm_model import *
+
+DEFAULT_PAST_COUNT = 24*30
+DEFAULT_FUTURE_COUNT = 24*10
+
+def main():
+	# parsing inputs
+	try:
+		argpastcount = DEFAULT_PAST_COUNT
+		argfuturecount = DEFAULT_FUTURE_COUNT
+
+		mflagidx = sys.argv.index('-m')
+		modelname = sys.argv[mflagidx+1]
+		cflagidx = sys.argv.index('-c')
+		argcurrencyname = sys.argv[cflagidx+1]
+		
+		if '-p' in sys.argv:
+			pflagidx = sys.arg.index('-p')
+			argpastcount = sys.argv[[pflagidx+1]]
+		if '-f' in sys.argv:
+			fflagidx = sys.argv.index('-f')
+			argdaycount = sys.argv[fflagidx+1]
+
+
+	except ValueError:
+		# malformed command line arguments, print usage and exit
+		print "usage: python ", sys.argv[0], "-m model.h5 -c nameOfCurrency -p pastcount -f futurecount"
+		print "-p and -f optional"
+		exit()
+
+	# get the past days data
+	conn = psycopg2.connect("host=localhost dbname=crypto user=postgres")
+	cursor = conn.cursor()
+	query = "SELECT " + ", ".join(TRAINING_ATTRIBUTES) + ", time FROM ("\
+	        + "SELECT " + ", ".join(TRAINING_ATTRIBUTES)+", time FROM bitcoin "\
+	        + " WHERE UPPER(Currency)=UPPER(\'"+currencyname+"\')"\
+	        + " ORDER BY Time DESC LIMIT "+str(argpastcount)\
+	        + ") as sub "\
+	        + " ORDER BY time ASC"
+	cursor.execute(query)
+	results = cursor.fetchall()
+	past_df = pd.DataFrame(results, columns=TRAINING_ATTRIBUTES+['time'])
+	conn.close()
+
+	# get time data
+	initial_time = past_df['time'][0]
+	past_df.drop(columns=['time'])
+	print initial_time
+	exit()
+
+	past_times = initial_time
+
+
+	# get the predicted future data
+	future_df = predict_future(inputmodelname = modelname,
+					currencyname = argcurrencyname,
+					daycount = argfuturecount)
+
+
+	# print past_df, future_df
+
+	plt.plot(past['time'], past['price'])
+	plt.plot(futr['time'], futr['price'])
+
+if __name__ is "__main__":
+	main()
